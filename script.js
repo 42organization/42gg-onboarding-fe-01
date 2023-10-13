cc1b9e7daf48a75bee4dc9007539e3345b55c630
greatSweetMango <jhkim0097@gmail.com>
2023-10-13 20:08:22 +0900

    버튼 추가

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
 const body = document.querySelector('body');
 const modal = document.querySelector('.modal');
 const btnOpenPopup = document.querySelector('.btn-open-popup');
+const btnAddTodo = document.querySelector('.btn-add-todo');
+const todoInput = document.querySelector('#todo-input');
+const todoList = document.querySelector('#todo-list');
+const doneList = document.querySelector('#done-list');
 
+//addTodo popup
 btnOpenPopup.addEventListener('click', () => {
   modal.classList.toggle('show');
 
@@ -19,3 +24,25 @@ modal.addEventListener('click', (event) => {
     }
   }
 });
+
+btnAddTodo.addEventListener('click', handleToDoSubmit);
+
+
+function handleToDoSubmit(event) {
+  event.preventDefault();
+  const newTodo = {
+    id : Date.now(),
+    content : todoInput.value,
+    is_done : false,
+  }
+  console.log('asdfasdfsda');
+  printTodoList(newTodo);
+  saveTodoData();
+}
+
+
+function printTodoList(todoData) {
+  let li = document.createElement('li');
+  li.innerText = todoData.content;
+  todoList.appendChild(li);
+}
