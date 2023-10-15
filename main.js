c06e3f26963caf9cf3e43931996588493975dae3
Clearsu <clearwater1219@gmail.com>
2023-10-15 14:05:55 +0900

    [Feat] default page #2

---
+++ main.js
@@ -0,0 +1,5 @@
+document.addEventListener('DOMContentLoaded', function () {
+    // 페이지가 로드되면 실행할 코드
+    const heading = document.querySelector('h1');
+    heading.textContent = 'Hello, JavaScript!';
+});
