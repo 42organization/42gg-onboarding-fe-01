491ebb1f30c95788ca348d83d13c3c9b080688e1
juha <contemplation.person@gmail.com>
2024-01-18 22:31:26 +0900

    [Feat] add button open

--- todo.js
+++ todo.js
@@ -115,3 +115,14 @@ function modifyTodo(id) {
     todoMap.set(key, value);
     render();
 }
+
+const fileInput = document.querySelector("input[type=file]");
+const output = document.querySelector(".output");
+
+fileInput.addEventListener("change", async () => {
+  const [file] = fileInput.files;
+
+  if (file) {
+    output.innerText = await file.text();
+  }
+});
