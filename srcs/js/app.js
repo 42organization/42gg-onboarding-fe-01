ec8fff25f1833c21acca8674aefbf690f73ad68d
Lee Jeong Ron <83465749+JeongRon@users.noreply.github.com>
2024-06-05 15:41:21 +0900

    [Refactor] 함수명 변경, 중복 코드 함수 처리, 이벤트 리스너 명시
    1. 함수명 변경
        - saveTodoList() → savesaveLocalStorage() : todoList 안의 데이터들을 로컬 스토리지에 넣기
        - createTodoList() → createTodo() : deleteTodo(), saveTodo() 통일성을 위해 변경
    2. 중복 코드 함수 처리
        - createTodoItemElement 함수 추가
    3. 이벤트 리스너 코드 리팩토링
        - (e) ⇒ {실행 코드} / 명시적으로 이벤트 발생 시, 특정 코드와 함수가 실행되게 설정

--- srcs/js/app.js
+++ srcs/js/app.js
@@ -1,52 +1,66 @@
 const TODOS_KEY = "todo";
 let todoList = [];
+loadLocalStorage();
 
-// localStorage에 todoList 저장하는 함수
-function saveTodoList() {
+// submit EventListener
+const todoForm = document.querySelector("form");
+todoForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const inputBox = document.querySelector(".todoListInputBox");
+  const newTodo = inputBox.value;
+  inputBox.value = "";
+  if (newTodo != "") {
+    const newTodoItem = {
+      text: newTodo,
+      id: Date.now(),
+    };
+    todoList.push(newTodoItem);
+    createTodo(newTodoItem);
+    saveLocalStorage();
+  }
+});
+
+// localStorage에서 데이터 가져오기 -> todoList에 데이터 추가 + UI 생성
+function loadLocalStorage() {
+  const getTodoList = localStorage.getItem(TODOS_KEY);
+  if (getTodoList !== null) {
+    const parsedTodoList = JSON.parse(getTodoList);
+    todoList = parsedTodoList;
+    parsedTodoList.forEach((todoItem) => {
+      createTodo(todoItem);
+    });
+  }
+}
+
+// todoList 데이터를 localStorage에 저장(갱신)
+function saveLocalStorage() {
   localStorage.setItem(TODOS_KEY, JSON.stringify(todoList));
 }
 
-function saveTodo(todoItemWrapper) {
-  return (e) => {
-    e.preventDefault();
-    const modifyInput = todoItemWrapper.querySelector("input");
-    const modifyTextContent = modifyInput.value;
-    const todoItem = todoList.find(
-      (item) => item.id === parseInt(todoItemWrapper.id)
-    );
-    todoItem.text = modifyTextContent;
-    saveTodoList();
-    todoItemWrapper.innerHTML = "";
+// todo UI 생성하는 함수
+function createTodo(todoItem) {
+  const todoListItemsContainer = document.querySelector(
+    ".todoListItemsContainer"
+  );
 
-    const todoItemText = document.createElement("p");
-    todoItemText.innerText = todoItem.text;
+  const todoItemWrapper = document.createElement("div");
+  todoItemWrapper.classList.add("todoListItemsWrapper");
+  todoItemWrapper.id = todoItem.id;
 
-    const modifyButton = document.createElement("button");
-    modifyButton.innerText = "MODIFY";
-    modifyButton.addEventListener("click", modifyTodo);
-
-    const deleteButton = document.createElement("button");
-    deleteButton.innerText = "DELETE";
-    deleteButton.addEventListener("click", deleteTodo);
-
-    todoItemWrapper.appendChild(todoItemText);
-    todoItemWrapper.appendChild(modifyButton);
-    todoItemWrapper.appendChild(deleteButton);
-  };
+  createTodoItemElement(todoItemWrapper, todoItem.text);
+  todoListItemsContainer.appendChild(todoItemWrapper);
 }
 
-function modifyTodo(e) {
-  const todoItemWrapper = e.target.parentElement;
-
+// 수정(modify) 버튼 클릭 시, 작동되는 함수
+function modifyTodo(todoItemWrapper) {
   const todoItemText = todoItemWrapper.querySelector("p");
   const todoTextContent = todoItemText.textContent;
-  const todoId = todoItemWrapper.id;
 
   const buttons = todoItemWrapper.querySelectorAll("button");
   const modifyButton = buttons[0];
   const deleteButton = buttons[1];
 
-  // input form, save, cancel
+  // modify UI 만들기
   const modifyForm = document.createElement("form");
   const modifyInput = document.createElement("input");
   modifyInput.classList.add("modifyInput");
@@ -63,76 +77,64 @@ function modifyTodo(e) {
   todoItemWrapper.replaceChild(saveButton, modifyButton);
   todoItemWrapper.replaceChild(cancelButton, deleteButton);
 
-  saveButton.addEventListener("click", saveTodo(todoItemWrapper));
-  modifyForm.addEventListener("submit", saveTodo(todoItemWrapper));
+  saveButton.addEventListener("click", (e) => {
+    e.preventDefault();
+    saveTodo(todoItemWrapper);
+  });
+  modifyForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+    saveTodo(todoItemWrapper);
+  });
   cancelButton.addEventListener("click", (e) => {
     todoItemWrapper.replaceChild(todoItemText, modifyForm);
     todoItemWrapper.replaceChild(modifyButton, saveButton);
     todoItemWrapper.replaceChild(deleteButton, cancelButton);
   });
+
   modifyInput.focus();
 }
 
-function deleteTodo(e) {
-  const todoItemWrapper = e.target.parentElement;
+// 삭제(delete) 버튼 클릭 시, 작동되는 함수
+function deleteTodo(todoItemWrapper) {
   todoItemWrapper.remove();
   todoList = todoList.filter(
     (item) => item.id !== parseInt(todoItemWrapper.id)
   );
-  saveTodoList();
+  saveLocalStorage();
 }
 
-const todoListItemsContainer = document.querySelector(
-  ".todoListItemsContainer"
-);
+// 저장(save) 버튼 클릭 시, 작동되는 함수
+function saveTodo(todoItemWrapper) {
+  const modifyInput = todoItemWrapper.querySelector("input");
+  const modifyTextContent = modifyInput.value;
+  const todoItem = todoList.find(
+    (item) => item.id === parseInt(todoItemWrapper.id)
+  );
+  todoItem.text = modifyTextContent;
+  saveLocalStorage();
+  todoItemWrapper.innerHTML = "";
+  createTodoItemElement(todoItemWrapper, todoItem.text);
+}
 
-// todoList UI 생성 함수
-function createTodoList(todoItem) {
-  const todoItemWrapper = document.createElement("div");
-  todoItemWrapper.classList.add("todoListItemsWrapper");
-  todoItemWrapper.id = todoItem.id;
-
+function createTodoItemElement(todoItemWrapper, textContent) {
   const todoItemText = document.createElement("p");
-  todoItemText.innerText = todoItem.text;
+  todoItemText.innerText = textContent;
 
   const modifyButton = document.createElement("button");
   modifyButton.innerText = "MODIFY";
-  modifyButton.addEventListener("click", modifyTodo);
+  modifyButton.addEventListener("click", (e) => {
+    const targetWrapper = e.target.parentElement;
+    modifyTodo(targetWrapper);
+  });
 
   const deleteButton = document.createElement("button");
   deleteButton.innerText = "DELETE";
-  deleteButton.addEventListener("click", deleteTodo);
+  deleteButton.addEventListener("click", (e) => {
+    const targetWrapper = e.target.parentElement;
+    deleteTodo(targetWrapper);
+  });
 
   todoItemWrapper.appendChild(todoItemText);
   todoItemWrapper.appendChild(modifyButton);
   todoItemWrapper.appendChild(deleteButton);
-  todoListItemsContainer.appendChild(todoItemWrapper);
 }
-
-// localStorage에 todoList 데이터 가져오기
-const getTodoList = localStorage.getItem(TODOS_KEY);
-if (getTodoList !== null) {
-  const parsedTodoList = JSON.parse(getTodoList);
-  todoList = parsedTodoList;
-  parsedTodoList.forEach((todoItem) => {
-    createTodoList(todoItem);
-  });
-}
-
-// submit EventListener
-const inputBox = document.querySelector(".todoListInputBox");
-const todoForm = document.querySelector("form");
-todoForm.addEventListener("submit", (e) => {
-  e.preventDefault();
-  const newTodo = inputBox.value;
-  inputBox.value = "";
-  if (newTodo != "") {
-    const newTodoItem = {
-      text: newTodo,
-      id: Date.now(),
-    };
-    todoList.push(newTodoItem);
-    createTodoList(newTodoItem);
-    saveTodoList();
-  }
-});
