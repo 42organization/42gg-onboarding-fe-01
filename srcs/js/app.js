6ac921040e3552ad3adc141812ca921af937d3cb
Lee Jeong Ron <83465749+JeongRon@users.noreply.github.com>
2024-06-05 14:13:51 +0900

    [Feat] todo리스트 수정하기, 삭제하기 기능 추가

--- srcs/js/app.js
+++ srcs/js/app.js
@@ -6,6 +6,82 @@ function saveTodoList() {
   localStorage.setItem(TODOS_KEY, JSON.stringify(todoList));
 }
 
+function saveTodo(todoItemWrapper) {
+  return (e) => {
+    e.preventDefault();
+    const modifyInput = todoItemWrapper.querySelector("input");
+    const modifyTextContent = modifyInput.value;
+    const todoItem = todoList.find(
+      (item) => item.id === parseInt(todoItemWrapper.id)
+    );
+    todoItem.text = modifyTextContent;
+    saveTodoList();
+    todoItemWrapper.innerHTML = "";
+
+    const todoItemText = document.createElement("p");
+    todoItemText.innerText = todoItem.text;
+
+    const modifyButton = document.createElement("button");
+    modifyButton.innerText = "MODIFY";
+    modifyButton.addEventListener("click", modifyTodo);
+
+    const deleteButton = document.createElement("button");
+    deleteButton.innerText = "DELETE";
+    deleteButton.addEventListener("click", deleteTodo);
+
+    todoItemWrapper.appendChild(todoItemText);
+    todoItemWrapper.appendChild(modifyButton);
+    todoItemWrapper.appendChild(deleteButton);
+  };
+}
+
+function modifyTodo(e) {
+  const todoItemWrapper = e.target.parentElement;
+
+  const todoItemText = todoItemWrapper.querySelector("p");
+  const todoTextContent = todoItemText.textContent;
+  const todoId = todoItemWrapper.id;
+
+  const buttons = todoItemWrapper.querySelectorAll("button");
+  const modifyButton = buttons[0];
+  const deleteButton = buttons[1];
+
+  // input form, save, cancel
+  const modifyForm = document.createElement("form");
+  const modifyInput = document.createElement("input");
+  modifyInput.classList.add("modifyInput");
+  modifyInput.value = todoTextContent;
+  modifyForm.appendChild(modifyInput);
+
+  const saveButton = document.createElement("button");
+  saveButton.innerText = "SAVE";
+
+  const cancelButton = document.createElement("button");
+  cancelButton.innerText = "CANCEL";
+
+  todoItemWrapper.replaceChild(modifyForm, todoItemText);
+  todoItemWrapper.replaceChild(saveButton, modifyButton);
+  todoItemWrapper.replaceChild(cancelButton, deleteButton);
+
+  saveButton.addEventListener("click", saveTodo(todoItemWrapper));
+  modifyForm.addEventListener("submit", saveTodo(todoItemWrapper));
+  cancelButton.addEventListener("click", (e) => {
+    todoItemWrapper.replaceChild(todoItemText, modifyForm);
+    todoItemWrapper.replaceChild(modifyButton, saveButton);
+    todoItemWrapper.replaceChild(deleteButton, cancelButton);
+  });
+  modifyInput.focus();
+}
+
+function deleteTodo(e) {
+  const todoItemWrapper = e.target.parentElement;
+  todoItemWrapper.remove();
+  todoList = todoList.filter(
+    (item) => item.id !== parseInt(todoItemWrapper.id)
+  );
+  saveTodoList();
+}
+
 const todoListItemsContainer = document.querySelector(
   ".todoListItemsContainer"
 );
@@ -14,15 +90,18 @@ const todoListItemsContainer = document.querySelector(
 function createTodoList(todoItem) {
   const todoItemWrapper = document.createElement("div");
   todoItemWrapper.classList.add("todoListItemsWrapper");
+  todoItemWrapper.id = todoItem.id;
 
   const todoItemText = document.createElement("p");
   todoItemText.innerText = todoItem.text;
 
   const modifyButton = document.createElement("button");
   modifyButton.innerText = "MODIFY";
+  modifyButton.addEventListener("click", modifyTodo);
 
   const deleteButton = document.createElement("button");
   deleteButton.innerText = "DELETE";
+  deleteButton.addEventListener("click", deleteTodo);
 
   todoItemWrapper.appendChild(todoItemText);
   todoItemWrapper.appendChild(modifyButton);
